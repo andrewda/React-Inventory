a4eaa5064f6fb5e74ace62e96e88fcfee561eeee
Evan Hopper-Moore <evanhoppermoore@gmail.com>
2017-05-20 15:22:23 -0700

    Add SideBar to MainPage

--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import Header from '../components/Header';
-import Tote from '../components/Tote'
+import SideBar from '../components/SideBar';
 
 import logo from '../logo.svg';
 
@@ -9,8 +9,8 @@ class MainPage extends Component {
     render() {
         return (
             <div className="App">
+                <SideBar />
                 <Header imageSource={logo} />
-                <Tote />
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
