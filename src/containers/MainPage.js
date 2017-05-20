d28500c0fd15b74596270fbf3426b43a7d58f6f8
daniwhite <white.dani.m@gmail.com>
2017-05-20 16:33:27 -0700

    Add grid to main page

--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -4,6 +4,7 @@ import Header from '../components/Header';
 import SideBar from '../components/SideBar';
 import AppBar from 'material-ui/AppBar';
 import ToteList from '../components/ToteList'
+import Grid from '../components/Grid'
 
 import logo from '../logo.svg';
 
@@ -13,9 +14,7 @@ class MainPage extends Component {
             <div className="App">
                 <SideBar />
                 <AppBar ishowMenuIconButton={false} />
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+                <Grid />
             </div>
         );
     }
