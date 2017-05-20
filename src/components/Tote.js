3cd556ec860f4ad2de0d12e40c6ccb24a2641573
verycumbersome <matthewsages@gmail.com>
2017-05-20 16:02:29 -0700

    Add tote object

--- src/components/Tote.js
+++ src/components/Tote.js
@@ -5,71 +5,19 @@ import IconButton from 'material-ui/IconButton';
 import Subheader from 'material-ui/Subheader';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 
-const styles = {
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-  },
-  gridList: {
-    width: 750,
-    height: 450,
-    overflowY: 'auto',
-  },
-};
+class Tote extends Component {
+    render() {
+        return (
+                <GridTile
+                  key={this.props.name}
+                  title={this.props.name}
+                  subtitle={<span>Last updated: <b>{this.props.lastupdate}</b></span>}
+                  actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+                >
+                  <img src={this.props.img} />
+                </GridTile>
+        )
+    }
+}
 
-const tilesData = [
-    {
-    img: 'images/tote.jpg',
-    title: 'tote1',
-    lastupdate: 'n/a',
-    },
-    {
-    img: 'images/tote.jpg',
-    title: 'tote2',
-    lastupdate: 'n/a',
-    },
-    {
-    img: 'images/tote.jpg',
-    title: 'tote3',
-    lastupdate: 'n/a',
-    },
-    {
-    img: 'images/tote.jpg',
-    title: 'tote4',
-    lastupdate: 'n/a',
-    },
-    {
-    img: 'images/tote.jpg',
-    title: 'tote5',
-    lastupdate: 'n/a',
-    },
-    {
-    img: 'images/tote.jpg',
-    title: 'tote6',
-    lastupdate: 'n/a',
-    },
-];
-
-const ToteList = () => (
-  <div style={styles.root}>
-    <Subheader><h1>Totes</h1></Subheader>
-    <GridList
-      cellHeight={180}
-      cols={3}
-      style={styles.gridList}
-    >
-      {tilesData.map((tile) => (
-        <GridTile
-          key={tile.title}
-          title={tile.title}
-          subtitle={<span>Last updated: <b>{tile.lastupdate}</b></span>}
-        >
-          <img src={tile.img} />
-        </GridTile>
-      ))}
-    </GridList>
-  </div>
-);
-
-export default ToteList;
+export default Tote;
