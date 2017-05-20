82fe3b3ebed1f056328d6a31abc74089a05d00d0
Andrew Dassonville <dassonville.andrew@gmail.com>
2017-05-20 13:58:35 -0700

    Fix const styling

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import * as firebase from 'firebase';
 
-var config = {
+const config = {
     apiKey: "AIzaSyAwwBdlVUGdmKh8hoQstTfceEoWwcgnHlE",
     authDomain: "frc-inventory.firebaseapp.com",
     databaseURL: "https://frc-inventory.firebaseio.com",
     projectId: "frc-inventory",
     storageBucket: "frc-inventory.appspot.com",
     messagingSenderId: "596898757396"
-  };
+};
 
 firebase.initializeApp(config);
 
