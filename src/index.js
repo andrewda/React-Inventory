aa3714f82065e78ddf73cd5b03146b971bec31dc
verycumbersome <matthewsages@gmail.com>
2017-05-20 13:22:28 -0700

    Add firebase ref

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyAwwBdlVUGdmKh8hoQstTfceEoWwcgnHlE",
+    authDomain: "frc-inventory.firebaseapp.com",
+    databaseURL: "https://frc-inventory.firebaseio.com",
+    projectId: "frc-inventory",
+    storageBucket: "frc-inventory.appspot.com",
+    messagingSenderId: "596898757396"
+  };
+
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
