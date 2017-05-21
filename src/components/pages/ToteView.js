92f8e6a05e722ed89f63e597a011ad46c74ab77e
Andrew Dassonville <dassonville.andrew@gmail.com>
2017-05-21 15:43:57 -0700

    Remove commented code

--- src/components/pages/ToteView.js
+++ src/components/pages/ToteView.js
@@ -21,27 +21,3 @@ const ToteView = () => (
 );
 
 export default ToteView;
-
-/*
-import React from 'react';
-import Paper from 'material-ui/Paper';
-
-import ItemsTable from '../ItemsTable';
-
-const style = {
-    position: 'relative',
-    width: '90%',
-    top: 25,
-    left: '5%'
-};
-
-const AllItems = (props) => (
-    <div className="AllItems page">
-        <Paper style={style}>
-            <ItemsTable items={props.items} />
-        </Paper>
-    </div>
-);
-
-export default AllItems;
-*/
