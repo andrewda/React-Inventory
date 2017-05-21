a174393dc9ddb56b38aa5a9e87066237f071af8e
Andrew Dassonville <dassonville.andrew@gmail.com>
2017-05-21 13:04:11 -0700

    Fix tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+
 import App from '../containers/App';
+
+import store from '../store';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>, div);
 });
